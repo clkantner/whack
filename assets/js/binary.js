33c481b888f843cc619e8ece28b1bde1655066ea
Claire Kantner <106267801+clkantner@users.noreply.github.com>
2022-07-29 14:12:46 -0700

    0.0.4

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.8.29/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.3","target":"arcade","targetVersion":"1.8.29","repo":"clkantner/whack"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.8.29/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.4","target":"arcade","targetVersion":"1.8.29","repo":"clkantner/whack"}
 // total=1207703 new=5.25% cached=89.10% other=5.65%
 (function (ectx) {
 'use strict';
@@ -406,7 +406,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P69326(s) {
+function _main___P69396(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -756,13 +756,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P69326.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P69326.continuations = [  ]
+_main___P69396.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P69396.continuations = [  ]
 
-function _main___P69326_mk(s) {
+function _main___P69396_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P69326, depth: s.depth + 1,
+        parent: s, fn: _main___P69396, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -40970,5 +40970,5 @@ const sprites_BaseSpriteSayRenderer__C53942_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P69326
+return _main___P69396
 })
